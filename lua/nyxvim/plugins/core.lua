ff80358e68d44ef8bfd65336a6388ec7d2537a46
Sahidul Islam <sahidul.net@gmail.com>
2023-07-22 19:16:51 +0600

    Initial commint

---
+++ lua/nyxvim/plugins/core.lua
@@ -0,0 +1,5 @@
+require("nyxvim.config").init()
+
+return {
+  { "folke/lazy.nvim", version = "*" },
+}
---
+++ lua/nyxvim/plugins/extras/editor/flash.lua
@@ -0,0 +1,6 @@
+require("lazy.core.util").warn(
+  "`flash.nvim` is now the default jump plugin for **NyxVim**.\nPlease remove the `nyxvim.plugins.extras.editor.flash` import from your **lazy** config.",
+  { title = "NyxVim" }
+)
+
+return {}
